2172c61baff9ca0290e3b4f5fc326ee5dbf0dc20
Steven Hall <stevenbryanb.work@gmail.com>
2023-06-09 17:37:33 +0100

    Trigger load scene

---
+++ Assets/Data/Scripts/Triggers/TriggerLoadScene.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+public class TriggerLoadScene : MonoBehaviour
+{ 
+   private GameManager _gameManager;
+   private int _levelNumber;
+   private void Awake()
+   {
+      _gameManager = FindObjectOfType<GameManager>(); 
+   }
+   
+   public void GetLevelNumber(int levelNumber)
+   {
+      _levelNumber = levelNumber;
+   }
+
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         _gameManager.NextLevel(_levelNumber);
+         
+      }
+   }
+}
