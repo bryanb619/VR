6220e607df4c47e333fde66c36d614396450b4c7
DiogoF22104684 <a22104684@alunos.ulht.pt>
2023-06-09 20:44:34 +0100

    Triggers and death condition

---
+++ Assets/Data/Scripts/Triggers/SignKnockdown.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignKnockdown : MonoBehaviour
+{
+    private int totalSigns;
+    private bool signsGotUp;
+    [SerializeField] private RailSystem railSystem;
+    private GameManager gameManager;
+    private void Start()
+    {
+        totalSigns = 0;
+        signsGotUp = false;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (signsGotUp && totalSigns == 0)
+        {
+            StopCoroutine(DeathTimer());
+            railSystem.ChangeDestination();
+            signsGotUp = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Sign"))
+        {
+            totalSigns += 1;
+            signsGotUp = true;
+            StartCoroutine(DeathTimer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Sign"))
+        {
+            totalSigns -= 1;
+        }
+    }
+
+    private IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(10f);
+        gameManager.EndScene();
+    }
+}
