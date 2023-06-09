89faf2b42279443815d8511eee19727c3046636f
Steven Hall <stevenbryanb.work@gmail.com>
2023-06-09 21:58:27 +0100

    Stops for Level 2

--- Assets/Data/Scripts/Rails/RailSystem.cs
+++ Assets/Data/Scripts/Rails/RailSystem.cs
@@ -7,6 +7,8 @@ public class RailSystem : MonoBehaviour
     [SerializeField] private Transform[] target;
 
     private Cart _cart;
+    
+    private int _i = 0;  
 
     private void Awake()
     {
@@ -16,7 +18,7 @@ public class RailSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _cart.Destination(target[0]);
+        StartPoint(); 
     }
 
     // Update is called once per frame
@@ -32,42 +34,75 @@ public class RailSystem : MonoBehaviour
         }   
         #endif
     }
+    
+    private void StartPoint()
+    {
+        _cart.Destination(target[0]);
+    }
 
     public void ChangeDestination()
     {
-        int i = 0;
-
-        for (i = 0; i < 10; i++)
+        _i++; 
+        
+        switch (_i)
         {
-            switch (i)
+            case 1:
             {
-                case 1:
-                    {
-                        _cart.Destination(target[1]);
-                        DestroyPoints(target[0]);
-                        break;
-                    }
-                case 2:
-                    {
-                        _cart.Destination(target[2]);
-                        DestroyPoints(target[1]);
-                        break;
-                    }
-                case 3:
-                    {
-                        _cart.Destination(target[3]);
+                if (target[0] != null)
+                {
+                    DestroyPoints(target[0]);
+                }
+                
+                _cart.Destination(target[1]);
+                break;
+            }
+            case 2:
+            {
+                
+                if (target[1] != null)
+                {
+                    DestroyPoints(target[1]);
+                }
+                _cart.Destination(target[2]);
 
-                        DestroyPoints(target[2]);
-                        break;
-                    }
-                case 4:
-                    {
-                        _cart.Destination(target[4]);
-                        DestroyPoints(target[3]);
-                        break;
-                    }
+                break;
+            }
+            case 3:
+            {
+                
+                if (target[2] != null)
+                {
+                    DestroyPoints(target[2]);
+                }
+                
+                _cart.Destination(target[3]);
+                
+                break;
+            }
+            case 4:
+            {
+                _cart.Destination(target[4]);
+                
+                if (target[3] != null)
+                {
+                    DestroyPoints(target[3]);
+                }
+                
+                break;
+            }
+            case 5:
+            {
+                
+                if (target[4] != null)
+                {
+                    DestroyPoints(target[4]);
+                }
+                _cart.Destination(target[5]);
+                break;
             }
         }
+
+        
     }
 
     private void DestroyPoints(Transform destroyTarget)
